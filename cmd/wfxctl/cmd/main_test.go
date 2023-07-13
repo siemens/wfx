3942128b06fa258633cf4346b4db9334b4ca1a53
Michael Adler <michael.adler@siemens.com>
2023-07-13 17:07:33 +0200

    test: ensure there are no leaks
    This commit ensures that each package is tested for leaks.
    
    Signed-off-by: Michael Adler <michael.adler@siemens.com>

---
+++ cmd/wfxctl/cmd/main_test.go
@@ -0,0 +1,19 @@
+package cmd
+
+/*
+ * SPDX-FileCopyrightText: 2023 Siemens AG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Author: Michael Adler <michael.adler@siemens.com>
+ */
+
+import (
+	"testing"
+
+	"go.uber.org/goleak"
+)
+
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m)
+}
