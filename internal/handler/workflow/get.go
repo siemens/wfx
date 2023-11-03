63ed9258cd23a1b8d7e8b82fb109dc4437dfb283
Michael Adler <michael.adler@siemens.com>
2023-11-03 17:07:39 +0100

    style: suppress error logging for non-existent workflows
    Logging errors should be reserved for situations warranting further
    investigation, such as issues with the persistent storage. A query for a
    (not yet) existing workflow is not an exceptional case and should not
    trigger error logging. This commit removes the unnecessary logging of
    such events.
    
    Signed-off-by: Michael Adler <michael.adler@siemens.com>

--- internal/handler/workflow/get.go
+++ internal/handler/workflow/get.go
@@ -12,18 +12,24 @@ import (
 	"context"
 
 	"github.com/Southclaws/fault"
+	"github.com/Southclaws/fault/ftag"
 	"github.com/siemens/wfx/generated/model"
 	"github.com/siemens/wfx/middleware/logging"
 	"github.com/siemens/wfx/persistence"
 )
 
 func GetWorkflow(ctx context.Context, storage persistence.Storage, name string) (*model.Workflow, error) {
-	log := logging.LoggerFromCtx(ctx)
+	log := logging.LoggerFromCtx(ctx).With().Str("name", name).Logger()
 	log.Debug().Str("name", name).Msg("Fetching workflow")
 	workflow, err := storage.GetWorkflow(ctx, name)
 	if err != nil {
-		log.Error().Err(err).Msg("Failed to get workflow from persistence storage")
+		if ftag.Get(err) == ftag.NotFound {
+			log.Debug().Msg("Workflow not found")
+		} else {
+			log.Error().Err(err).Msg("Failed to get workflow from storage")
+		}
 		return nil, fault.Wrap(err)
 	}
+	log.Debug().Msg("Found workflow")
 	return workflow, nil
 }
