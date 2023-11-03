1feb266ae595171c8ac99a7345d07aa04d0f6167
Michael Adler <michael.adler@siemens.com>
2023-11-03 17:07:39 +0100

    style: suppress error logging for non-existent workflows
    Logging errors should be reserved for situations warranting further
    investigation, such as issues with the persistent storage. A query for a
    (not yet) existing workflow is not an exceptional case and should not
    trigger error logging. This commit removes the unnecessary logging of
    such events.
    
    Signed-off-by: Michael Adler <michael.adler@siemens.com>

--- internal/handler/workflow/get_test.go
+++ internal/handler/workflow/get_test.go
@@ -10,8 +10,12 @@ package workflow
 
 import (
 	"context"
+	"errors"
 	"testing"
 
+	"github.com/Southclaws/fault"
+	"github.com/Southclaws/fault/ftag"
+	"github.com/siemens/wfx/persistence"
 	"github.com/siemens/wfx/workflow/dau"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -25,4 +29,26 @@ func TestGetWorkflow(t *testing.T) {
 	wf2, err := GetWorkflow(context.Background(), db, wf.Name)
 	assert.NoError(t, err)
 	assert.Equal(t, wf.Name, wf2.Name)
+}
+
+func TestGetWorkflow_NotFound(t *testing.T) {
+	ctx := context.Background()
+
+	db := persistence.NewMockStorage(t)
+	db.EXPECT().GetWorkflow(ctx, "foo").Return(nil, fault.Wrap(errors.New("Not found"), ftag.With(ftag.NotFound)))
+
+	wf, err := GetWorkflow(ctx, db, "foo")
+	assert.Nil(t, wf)
+	assert.NotNil(t, err)
+}
+
+func TestGetWorkflow_Internal(t *testing.T) {
+	ctx := context.Background()
+
+	db := persistence.NewMockStorage(t)
+	db.EXPECT().GetWorkflow(ctx, "foo").Return(nil, fault.Wrap(errors.New("Not found"), ftag.With(ftag.Internal)))
+
+	wf, err := GetWorkflow(ctx, db, "foo")
+	assert.Nil(t, wf)
+	assert.NotNil(t, err)
 }
