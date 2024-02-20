9504169877100be473b27ffeb20c14923d7f8bd2
Michael Adler <michael.adler@siemens.com>
2024-02-20 09:24:26 +0100

    test(wfx-viewer): add test case for plantuml generator
    Signed-off-by: Michael Adler <michael.adler@siemens.com>

--- cmd/wfx-viewer/output/plantuml/plantuml.go
+++ cmd/wfx-viewer/output/plantuml/plantuml.go
@@ -12,7 +12,6 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/rs/zerolog/log"
 	"github.com/siemens/wfx/cmd/wfx-viewer/colors"
 	"github.com/siemens/wfx/generated/model"
 	"github.com/spf13/pflag"
@@ -27,14 +26,7 @@ func NewGenerator() *Generator {
 func (g *Generator) RegisterFlags(_ *pflag.FlagSet) {}
 
 func (g *Generator) Generate(out io.Writer, workflow *model.Workflow) error {
-	mustWrite := func(s string) {
-		_, err := out.Write([]byte(s))
-		if err != nil {
-			log.Fatal().Err(err).Msg("Failed writing string")
-		}
-	}
-
-	mustWrite("@startuml\n")
+	_, _ = out.Write([]byte("@startuml\n"))
 
 	allStates := make(map[string]*model.State, len(workflow.States))
 	for _, state := range workflow.States {
@@ -45,29 +37,29 @@ func (g *Generator) Generate(out io.Writer, workflow *model.Workflow) error {
 
 	for _, state := range workflow.States {
 		fgColor, bgColor := cp.StateColor(state.Name)
-		mustWrite(fmt.Sprintf("state %s as \"<color:%s>%s</color>\" %s: %s\n", state.Name, fgColor, state.Name, bgColor, state.Description))
+		_, _ = out.Write([]byte((fmt.Sprintf("state %s as \"<color:%s>%s</color>\" %s: %s\n", state.Name, fgColor, state.Name, bgColor, state.Description))))
 	}
 
 	// add transitions
 	for _, transition := range workflow.Transitions {
-		mustWrite(fmt.Sprintf("%s --> %s: %s", transition.From, transition.To, string(transition.Eligible)))
+		_, _ = out.Write([]byte((fmt.Sprintf("%s --> %s: %s", transition.From, transition.To, string(transition.Eligible)))))
 		if string(transition.Action) != "" {
-			mustWrite(fmt.Sprintf(" [%s]", string(transition.Action)))
+			_, _ = out.Write([]byte((fmt.Sprintf(" [%s]", string(transition.Action)))))
 		}
-		mustWrite("\n")
+		_, _ = out.Write([]byte("\n"))
 	}
 
 	// add legend
-	mustWrite("legend right\n")
-	mustWrite("  | Color | Group | Description |\n")
+	_, _ = out.Write([]byte("legend right\n"))
+	_, _ = out.Write([]byte(("  | Color | Group | Description |\n")))
 	for _, group := range workflow.Groups {
 		color := cp.GroupColor(group.Name)
 		hex := color.ToHEX().String()
-		mustWrite(fmt.Sprintf("  | <%s> | %s | %s |\n", hex, group.Name, group.Description))
+		_, _ = out.Write([]byte((fmt.Sprintf("  | <%s> | %s | %s |\n", hex, group.Name, group.Description))))
 	}
-	mustWrite(fmt.Sprintf("  | <%s> | %s | %s |\n", colors.DefaultBgColor, "", "The state doesn't belong to any group."))
-	mustWrite("endlegend\n")
+	_, _ = out.Write([]byte((fmt.Sprintf("  | <%s> | %s | %s |\n", colors.DefaultBgColor, "", "The state doesn't belong to any group."))))
+	_, _ = out.Write([]byte(("endlegend\n")))
 
-	mustWrite("@enduml\n")
+	_, _ = out.Write([]byte(("@enduml\n")))
 	return nil
 }
