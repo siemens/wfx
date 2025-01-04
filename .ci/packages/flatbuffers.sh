e22b14af9d397e04e896fb0b052d0ccd874ab0d7
Michael Adler <michael.adler@siemens.com>
2025-01-04 11:19:16 +0100

    ci: fix flatbuffers download url
    Signed-off-by: Michael Adler <michael.adler@siemens.com>

--- .ci/packages/flatbuffers.sh
+++ .ci/packages/flatbuffers.sh
@@ -11,5 +11,5 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 . "$SCRIPT_DIR/versions.env"
 
 echo "Installing flatbuffers $FLATBUFFERS_VERSION"
-curl -Lo /tmp/flatc.zip "https://github.com/google/flatbuffers/releases/download/v${FLATBUFFERS_VERSION}/Linux.flatc.binary.clang++-15.zip"
+curl -Lo /tmp/flatc.zip "https://github.com/google/flatbuffers/releases/download/v${FLATBUFFERS_VERSION}/Linux.flatc.binary.clang++-18.zip"
 unzip /tmp/flatc.zip flatc -d /usr/local/bin/
