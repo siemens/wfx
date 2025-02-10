cd6fdb02693f0554f36f6bc54fab796a38f4495c
Michael Adler <michael.adler@siemens.com>
2025-02-10 13:38:54 +0100

    feat: add build tags to --version output
    Simplify inspection of a deployed wfx binary by including additional
    build context at a glance.
    
    Signed-off-by: Michael Adler <michael.adler@siemens.com>

---
+++ cmd/wfx/cmd/root/tag_plugin.go
@@ -0,0 +1,15 @@
+//go:build plugin
+
+/*
+ * SPDX-FileCopyrightText: 2025 Siemens AG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Author: Michael Adler <michael.adler@siemens.com>
+ */
+
+package root
+
+func init() {
+	buildTags = append(buildTags, "plugin")
+}
