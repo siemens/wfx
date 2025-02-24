be3b3873baf2decf8a8de24b28628e6f5a9740a7
Michael Adler <michael.adler@siemens.com>
2025-02-24 13:54:38 +0100

    feat: add journald-friendly log format
    Pass structured log data to journald using its native protocol [1].
    
    [1] https://systemd.io/JOURNAL_NATIVE_PROTOCOL/
    
    Signed-off-by: Michael Adler <michael.adler@siemens.com>

--- cmd/wfx/cmd/root/setup.go
+++ cmd/wfx/cmd/root/setup.go
@@ -13,7 +13,9 @@ import (
 	"os"
 	"time"
 
+	"github.com/coreos/go-systemd/v22/journal"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/journald"
 	"github.com/rs/zerolog/log"
 	"golang.org/x/term"
 )
@@ -26,19 +28,27 @@ func setupLogging(out *os.File, format string, lvl zerolog.Level) {
 		if term.IsTerminal(int(out.Fd())) {
 			format = "pretty"
 		} else {
-			format = "json"
+			if ok, _ := journal.StderrIsJournalStream(); ok {
+				format = "journald"
+			} else {
+				format = "json"
+			}
 		}
 	}
 	switch format {
 	case "json":
-		logger = zerolog.New(out).With().Caller().Logger()
+		logger = zerolog.New(out)
 	case "pretty":
 		logger = zerolog.New(zerolog.ConsoleWriter{
 			Out:        out,
 			TimeFormat: time.Stamp,
 		})
+	case "journald":
+		logger = zerolog.New(journald.NewJournalDWriter())
 	default:
-		fmt.Fprintf(os.Stderr, "Invalid log format: %s\n", format)
+		fmt.Fprintf(os.Stderr, "Invalid log format specified: %s\n", format)
 	}
 	log.Logger = logger.With().Timestamp().Caller().Logger()
+
+	log.Debug().Str("format", format).Msg("Logging configured successfully")
 }
