siemens/wfx

commit f9825d67c1652e2b36cab526d7aa7d9bcd85ce85

feat!: Upgrade to OpenAPI v3
Upgrading from Swagger to OpenAPI v3 offers numerous benefits, including
enhanced specification features, better support for modern use cases,
improved developer experience, access to a richer ecosystem and tooling,
future-proofing our API specifications, and ensuring
compliance and interoperability.

This change replaces the existing code generator, resulting in changes
to the generated model. Users of the Go client library will need to make
minor adjustments to their code.

Signed-off-by: Michael Adler <michael.adler@siemens.com>

cmd/wfx/cmd/root/server_collection.go
@@ -1,0 +1,383 @@
+package root
+
+/*
+ * SPDX-FileCopyrightText: 2024 Siemens AG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Author: Michael Adler <michael.adler@siemens.com>
+ */
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/Southclaws/fault"
+	"github.com/coreos/go-systemd/v22/activation"
+	nethttpmiddleware "github.com/oapi-codegen/nethttp-middleware"
+	"github.com/rs/cors"
+	"github.com/rs/zerolog/log"
+	"github.com/siemens/wfx/api"
+	"github.com/siemens/wfx/cmd/wfx/cmd/config"
+	"github.com/siemens/wfx/cmd/wfxctl/errutil"
+	genApi "github.com/siemens/wfx/generated/api"
+	"github.com/siemens/wfx/internal/handler/job/events"
+	"github.com/siemens/wfx/internal/server"
+	"github.com/siemens/wfx/middleware/logging"
+	"github.com/siemens/wfx/middleware/plugin"
+	"github.com/siemens/wfx/persistence"
+	"github.com/siemens/wfx/spec"
+	"golang.org/x/sync/errgroup"
+)
+
+type ServerCollection struct {
+	once    sync.Once
+	cfg     *config.AppConfig
+	storage persistence.Storage
+	north   *http.Server
+	south   *http.Server
+	wfx     *api.WfxServer
+
+	pluginMWs    []*plugin.Middleware
+	pluginErrors []<-chan error
+}
+
+func NewServerCollection(cfg *config.AppConfig, storage persistence.Storage) (*ServerCollection, error) {
+	wfx := api.NewWfxServer(storage)
+
+	swag, _ := genApi.GetSwagger()
+	validator := nethttpmiddleware.OapiRequestValidatorWithOptions(swag,
+		&nethttpmiddleware.Options{SilenceServersWarning: true})
+	corsMW := cors.AllowAll().Handler
+	logMW := logging.NewLoggingMiddleware()
+
+	// LIFO
+	middlewares := []genApi.MiddlewareFunc{validator, corsMW, logMW}
+
+	pluginMWs := make([]*plugin.Middleware, 0)
+	pluginErrors := make([]<-chan error, 0)
+
+	northPluginMWs, err := createPluginMiddlewares(cfg.MgmtPluginsDir())
+	if err != nil {
+		return nil, fault.Wrap(err)
+	}
+
+	for _, mw := range northPluginMWs {
+		pluginMWs = append(pluginMWs, mw)
+		pluginErrors = append(pluginErrors, mw.Errors())
+	}
+
+	northServer, err := createServer(cfg, api.NewNorthboundServer(wfx), middlewares, northPluginMWs)
+	if err != nil {
+		return nil, fault.Wrap(err)
+	}
+
+	southPluginMWs, err := createPluginMiddlewares(cfg.ClientPluginsDir())
+	if err != nil {
+		return nil, fault.Wrap(err)
+	}
+
+	for _, mw := range southPluginMWs {
+		pluginMWs = append(pluginMWs, mw)
+		pluginErrors = append(pluginErrors, mw.Errors())
+	}
+
+	southServer, err := createServer(cfg, api.NewSouthboundServer(wfx), middlewares, southPluginMWs)
+	if err != nil {
+		return nil, fault.Wrap(err)
+	}
+
+	return &ServerCollection{
+		cfg:          cfg,
+		storage:      storage,
+		pluginMWs:    pluginMWs,
+		pluginErrors: pluginErrors,
+		wfx:          wfx,
+		north:        northServer,
+		south:        southServer,
+	}, nil
+}
+
+func (sc *ServerCollection) Start() error {
+	sc.wfx.Start()
+
+	cfg := sc.cfg
+	schemes := cfg.Schemes()
+	// check for socket-based activation; order of sockets: south, north
+	systemdListeners, _ := activation.Listeners()
+	if len(systemdListeners) > 0 {
+		// perform sanity checks
+		if len(systemdListeners) != 2 {
+			return errors.New("systemd socket-based activation requires two sockets")
+		}
+		if len(schemes) != 1 || schemes[0] != config.SchemeUnix {
+			return errors.New("systemd socket-based activation only supports unix scheme")
+		}
+	}
+
+	var g errgroup.Group
+	for _, scheme := range schemes {
+		var northListener, southListener net.Listener
+		if len(systemdListeners) > 0 {
+			log.Debug().Msg("Using sockets provided by systemd")
+			southListener, northListener = systemdListeners[0], systemdListeners[1]
+		} else {
+			var err error
+			northSettings := ListenerSettings{
+				Host:    cfg.MgmtHost(),
+				Port:    cfg.MgmtPort(),
+				TLSHost: cfg.MgmtTLSHost(),
+				TLSPort: cfg.MgmtTLSPort(),
+				UDSPath: cfg.MgmtUnixSocket(),
+			}
+			northListener, err = createListener(scheme, northSettings)
+			if err != nil {
+				return fault.Wrap(err)
+			}
+
+			southSettings := ListenerSettings{
+				Host:    cfg.ClientHost(),
+				Port:    cfg.ClientPort(),
+				TLSHost: cfg.ClientTLSHost(),
+				TLSPort: cfg.ClientTLSPort(),
+				UDSPath: cfg.ClientUnixSocket(),
+			}
+			southListener, err = createListener(scheme, southSettings)
+			if err != nil {
+				return fault.Wrap(err)
+			}
+		}
+
+		isTLS := scheme == config.SchemeHTTPS
+		g.Go(func() error {
+			defer func() {
+				log.Debug().Msg("Northbound goroutine finished")
+			}()
+			log.Info().
+				Bool("tls", isTLS).
+				Str("scheme", scheme.String()).
+				Str("addr", northListener.Addr().String()).
+				Msg("Starting northbound server")
+
+			var err error
+			if isTLS {
+				err = sc.north.ServeTLS(northListener, cfg.TLSCertificate(), cfg.TLSKey())
+			} else {
+				err = sc.north.Serve(northListener)
+			}
+			if err != nil && !errors.Is(err, http.ErrServerClosed) {
+				log.Err(err).Msg("Northbound server encountered an error")
+				return fault.Wrap(err)
+			}
+			return nil
+		})
+
+		g.Go(func() error {
+			defer func() {
+				log.Debug().Msg("Southbound goroutine finished")
+			}()
+			log.Info().
+				Bool("tls", isTLS).
+				Str("scheme", scheme.String()).
+				Str("addr", southListener.Addr().String()).
+				Msg("Starting southbound server")
+			var err error
+			if isTLS {
+				err = sc.south.ServeTLS(southListener, cfg.TLSCertificate(), cfg.TLSKey())
+			} else {
+				err = sc.south.Serve(southListener)
+			}
+			if err != nil && !errors.Is(err, http.ErrServerClosed) {
+				log.Err(err).Msg("Southbound server encountered an error")
+				return fault.Wrap(err)
+			}
+			return nil
+		})
+	}
+
+	if len(sc.pluginErrors) > 0 {
+		g.Go(func() error {
+			defer func() {
+				log.Debug().Msg("Plugin reaper finished, triggering shutdown")
+				sc.Stop()
+			}()
+
+			running := true
+			for running {
+				for _, chErr := range sc.pluginErrors {
+					select {
+					case err := <-chErr:
+						running = false
+						if err != nil {
+							log.Err(err).Msg("Received plugin error")
+							return err
+						}
+					default:
+						// no errors or channel was closed
+					}
+				}
+				time.Sleep(time.Millisecond * 300)
+			}
+			return nil
+		})
+	}
+
+	log.Debug().Msg("Waiting for goroutines to finish")
+	err := g.Wait()
+	log.Debug().Err(err).Msg("Goroutines finished")
+
+	return fault.Wrap(err)
+}
+
+// Stop the server collection and its associated listeners. It's safe to call this method multiple times.
+func (sc *ServerCollection) Stop() {
+	sc.once.Do(func() {
+		timeout := sc.cfg.GracefulTimeout()
+		log.Info().Dur("timeout", timeout).Msg("Shutting down server collection")
+
+		// shut down (disconnect) subscribers otherwise we cannot stop the web server due to open connections
+		events.ShutdownSubscribers()
+
+		var shutdownGroup sync.WaitGroup
+		if sc.north != nil {
+			log.Debug().Msg("Shutting down northbound servers")
+			shutdownGroup.Add(1)
+			go func() {
+				defer shutdownGroup.Done()
+
+				timeoutCtx, timeoutCancel := context.WithTimeout(context.Background(), timeout)
+				defer timeoutCancel()
+
+				_ = sc.north.Shutdown(timeoutCtx)
+				log.Debug().Msg("Northbound server shut down complete")
+			}()
+		}
+		if sc.south != nil {
+			log.Debug().Msg("Shutting down southbound servers")
+			shutdownGroup.Add(1)
+			go func() {
+				defer shutdownGroup.Done()
+
+				timeoutCtx, timeoutCancel := context.WithTimeout(context.Background(), timeout)
+				defer timeoutCancel()
+
+				_ = sc.south.Shutdown(timeoutCtx)
+				log.Debug().Msg("Southbound server shut down complete")
+			}()
+		}
+		shutdownGroup.Wait()
+
+		log.Debug().Msg("Shutting down plugin middlewares")
+		for _, mw := range sc.pluginMWs {
+			mw.Stop()
+		}
+
+		log.Debug().Msg("Shutting down wfx")
+		sc.wfx.Stop()
+		log.Debug().Msg("Finished shutting down wfx")
+
+		log.Info().Msg("Server collection shut down complete")
+	})
+}
+
+func createServer(cfg *config.AppConfig, ssi genApi.StrictServerInterface, baseMWs []genApi.MiddlewareFunc, pluginMWs []*plugin.Middleware) (*http.Server, error) {
+	combinedMWs := make([]genApi.MiddlewareFunc, 0, len(baseMWs)+len(pluginMWs))
+	_ = copy(combinedMWs, baseMWs)
+	for _, mw := range pluginMWs {
+		combinedMWs = append(combinedMWs, mw.Middleware())
+	}
+
+	swag, _ := genApi.GetSwagger()
+	basePath := errutil.Must(swag.Servers.BasePath())
+	strictHandler := genApi.NewStrictHandler(ssi, nil)
+	handler := genApi.HandlerWithOptions(strictHandler, genApi.StdHTTPServerOptions{
+		BaseURL:     basePath,
+		BaseRouter:  createMux(cfg, strictHandler),
+		Middlewares: combinedMWs,
+	})
+	server, err := server.NewHTTPServer(cfg, handler)
+	return server, fault.Wrap(err)
+}
+
+func createPluginMiddlewares(pluginDir string) ([]*plugin.Middleware, error) {
+	plugins, err := loadPlugins(pluginDir)
+	if err != nil {
+		return nil, fault.Wrap(err)
+	}
+
+	pluginMWs := make([]*plugin.Middleware, 0, len(plugins))
+	for _, p := range plugins {
+		chErr := make(chan error, 1)
+		mw, err := plugin.NewMiddleware(p, chErr)
+		if err != nil {
+			return nil, fault.Wrap(err)
+		}
+		pluginMWs = append(pluginMWs, mw)
+	}
+	return pluginMWs, nil
+}
+
+type ListenerSettings struct {
+	Host    string
+	Port    int
+	TLSHost string
+	TLSPort int
+	UDSPath string
+}
+
+func createListener(scheme config.Scheme, settings ListenerSettings) (net.Listener, error) {
+	var network, addr string
+	switch scheme {
+	case config.SchemeUnix:
+		network = "unix"
+		addr = settings.UDSPath
+	case config.SchemeHTTP:
+		network = "tcp"
+		addr = fmt.Sprintf("%s:%d", settings.Host, settings.Port)
+	case config.SchemeHTTPS:
+		network = "tcp"
+		addr = fmt.Sprintf("%s:%d", settings.TLSHost, settings.TLSPort)
+	default:
+		return nil, fmt.Errorf("unsupported scheme: %s", scheme)
+	}
+	contextLogger := log.With().Str("network", network).Str("addr", addr).Str("scheme", scheme.String()).Logger()
+	for attempt := 0; attempt < 30; attempt++ {
+		ln, err := net.Listen(network, addr)
+		if err == nil {
+			contextLogger.Debug().Msg("Created new listener")
+			return ln, nil
+		}
+		contextLogger.Err(err).Msg("Failed to create listener")
+		time.Sleep(time.Second)
+	}
+	return nil, errors.New("failed to create listener")
+}
+
+func createMux(cfg *config.AppConfig, server genApi.ServerInterface) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /version", func(w http.ResponseWriter, r *http.Request) { server.GetVersion(w, r) })
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) { server.GetHealth(w, r) })
+	mux.HandleFunc("GET /download/", func(w http.ResponseWriter, r *http.Request) {
+		rootDir := cfg.SimpleFileserver()
+		enabled := rootDir != ""
+		log.Debug().Bool("enabled", enabled).Msg("Received download request")
+		if enabled {
+			http.StripPrefix("/download", http.FileServer(http.Dir(rootDir))).ServeHTTP(w, r)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+			w.Header().Set("Content-Type", "application/json")
+			_, _ = w.Write([]byte(`{"code":404,"message":"path /download is not served as simple file server is not enabled"}`))
+		}
+	})
+
+	for pattern, handler := range spec.Handlers {
+		mux.Handle(pattern, handler)
+	}
+
+	return mux
+}

cmd/wfx/cmd/root/server_collection_test.go
@@ -1,0 +1,95 @@
+package root
+
+/*
+ * SPDX-FileCopyrightText: 2024 Siemens AG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Author: Michael Adler <michael.adler@siemens.com>
+ */
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/siemens/wfx/cmd/wfx/cmd/config"
+	"github.com/siemens/wfx/persistence"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewServerCollection(t *testing.T) {
+	dbMock := persistence.NewHealthyMockStorage(t)
+	sc, err := NewServerCollection(new(config.AppConfig), dbMock)
+	assert.NotNil(t, sc)
+	assert.NoError(t, err)
+}
+
+func TestOpenAPIJSON(t *testing.T) {
+	mux := createMux(new(config.AppConfig), nil)
+	rec := httptest.NewRecorder()
+	mux.ServeHTTP(rec, httptest.NewRequest(http.MethodGet, "/api/wfx/v1/openapiv3.json", nil))
+	result := rec.Result()
+	assert.Equal(t, http.StatusOK, result.StatusCode)
+}
+
+func TestTopLevelNotFound(t *testing.T) {
+	mux := createMux(new(config.AppConfig), nil)
+	rec := httptest.NewRecorder()
+	mux.ServeHTTP(rec, httptest.NewRequest(http.MethodGet, "/", nil))
+	result := rec.Result()
+	assert.Equal(t, http.StatusNotFound, result.StatusCode)
+	b, _ := io.ReadAll(result.Body)
+	assert.Equal(t, "The requested resource could not be found.\n\nHint: Check /api/wfx/v1/openapiv3.json to see available endpoints.\n", string(b))
+}
+
+func TestDownloadRedirect(t *testing.T) {
+	mux := createMux(new(config.AppConfig), nil)
+	rec := httptest.NewRecorder()
+	mux.ServeHTTP(rec, httptest.NewRequest(http.MethodGet, "/download", nil))
+	result := rec.Result()
+	assert.Equal(t, http.StatusMovedPermanently, result.StatusCode)
+	b, _ := io.ReadAll(result.Body)
+	assert.Contains(t, string(b), "/download/")
+}
+
+func TestDownload(t *testing.T) {
+	tmp := os.TempDir()
+	tmpFile, _ := os.CreateTemp(tmp, "TestDownload.*")
+	_, _ = tmpFile.Write([]byte("hello world"))
+	_ = tmpFile.Close()
+	t.Cleanup(func() { _ = os.Remove(tmpFile.Name()) })
+
+	f := config.NewFlagset()
+	_ = f.Parse([]string{"--" + config.SimpleFileServerFlag, tmp})
+	cfg, err := config.NewAppConfig(f)
+	require.NotEmpty(t, cfg.SimpleFileserver())
+	require.NoError(t, err)
+	t.Cleanup(cfg.Stop)
+
+	mux := createMux(cfg, nil)
+	rec := httptest.NewRecorder()
+	mux.ServeHTTP(rec, httptest.NewRequest(http.MethodGet, fmt.Sprintf("/download/%s", path.Base(tmpFile.Name())), nil))
+	result := rec.Result()
+	assert.Equal(t, http.StatusOK, result.StatusCode)
+	b, _ := io.ReadAll(result.Body)
+	assert.Contains(t, string(b), "hello world")
+}
+
+func TestDownload_NotFound(t *testing.T) {
+	f := config.NewFlagset()
+	cfg, err := config.NewAppConfig(f)
+	t.Cleanup(cfg.Stop)
+	require.NoError(t, err)
+	mux := createMux(cfg, nil)
+
+	rec := httptest.NewRecorder()
+	mux.ServeHTTP(rec, httptest.NewRequest(http.MethodGet, "/download/", nil))
+	result := rec.Result()
+	assert.Equal(t, http.StatusNotFound, result.StatusCode)
+}

commit c2677674279ade46e8b4a1dd8ffe16f72059f66d

fix: baseMWs are not called
The base middlewares (baseMWs) were not being invoked due to an
incorrect use of the copy() function, which only copies elements up to
the length of the destination slice. Since the destination slice was
initialized with zero length, no elements were copied. This has been
fixed by appending each middleware function individually to the
combinedMWs slice.

Fixes: f9825d67c1652e2b36cab526d7aa7d9bcd85ce85
Signed-off-by: Michael Adler <michael.adler@siemens.com>

cmd/wfx/cmd/root/server_collection.go
@@ -287,7 +287,7 @@ func (sc *ServerCollection) Stop() {
 
 func createServer(cfg *config.AppConfig, ssi genApi.StrictServerInterface, baseMWs []genApi.MiddlewareFunc, pluginMWs []*plugin.Middleware) (*http.Server, error) {
 	combinedMWs := make([]genApi.MiddlewareFunc, 0, len(baseMWs)+len(pluginMWs))
-	_ = copy(combinedMWs, baseMWs)
+	combinedMWs = append(combinedMWs, baseMWs...)
 	for _, mw := range pluginMWs {
 		combinedMWs = append(combinedMWs, mw.Middleware())
 	}

cmd/wfx/cmd/root/server_collection_test.go
@@ -9,6 +9,7 @@ package root
  */
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"net/http"
@@ -16,11 +17,15 @@ import (
 	"os"
 	"path"
 	"strings"
+	"sync/atomic"
 	"testing"
 
+	"github.com/siemens/wfx/api"
 	"github.com/siemens/wfx/cmd/wfx/cmd/config"
+	genAPI "github.com/siemens/wfx/generated/api"
 	"github.com/siemens/wfx/persistence"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
@@ -29,6 +34,31 @@ func TestNewServerCollection(t *testing.T) {
 	sc, err := NewServerCollection(new(config.AppConfig), dbMock)
 	assert.NotNil(t, sc)
 	assert.NoError(t, err)
+}
+
+func TestCreateServer_UseMiddlewares(t *testing.T) {
+	dbMock := persistence.NewHealthyMockStorage(t)
+	dbMock.EXPECT().QueryJobs(context.Background(), mock.Anything, mock.Anything, mock.Anything).Return(new(genAPI.PaginatedJobList), nil)
+	wfx := api.NewWfxServer(dbMock)
+
+	var myMWCalled atomic.Bool
+	myMW := func(next http.Handler) http.Handler {
+		myMWCalled.Store(true)
+		return next
+	}
+
+	middlewares := []genAPI.MiddlewareFunc{myMW}
+	server, err := createServer(new(config.AppConfig), api.NewNorthboundServer(wfx), middlewares, nil)
+	require.NoError(t, err)
+
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest("", "/api/wfx/v1/jobs", nil)
+
+	server.Handler.ServeHTTP(rec, req)
+
+	assert.Equal(t, http.StatusOK, rec.Result().StatusCode)
+
+	assert.True(t, myMWCalled.Load())
 }
 
 func TestOpenAPIJSON(t *testing.T) {