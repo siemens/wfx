siemens/wfx

commit 976875fab380ef85ece8c3b835a3ae4e891f9e70

feat: instant job event notifications
This commit introduces a more efficient mechanism for notifications
related to job events, such as creation, updating, and deletion. Unlike
the previous architecture, where clients had to constantly poll the
server for updates, the new implementation uses server-sent events (SSE)
to allow clients to receive updates from the server, improving
efficiency and reducing network overhead.

Key Features:

- Introduced a new SSE-enabled endpoint, `/jobs/events`, which accepts
  filter parameters to customize the stream of events received.
- Clients can subscribe to `/jobs/events` to receive updates on job
  events that meet specified filter criteria.
- Provided a client reference implementation through `wfxctl`.

Signed-off-by: Michael Adler <michael.adler@siemens.com>

middleware/logging/writer.go
@@ -43,3 +43,10 @@ func (w *responseWriter) Write(b []byte) (int, error) {
 	n, err := w.bodyWriter.Write(b)
 	return n, fault.Wrap(err)
 }
+
+// Flush implements the http.Flusher interface.
+// This is used by the server-sent events implementation to flush a single event to the client.
+func (w *responseWriter) Flush() {
+	flusher := w.httpWriter.(http.Flusher)
+	flusher.Flush()
+}

api/job_events_test.go
@@ -1,0 +1,135 @@
+package api
+
+/*
+ * SPDX-FileCopyrightText: 2023 Siemens AG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Author: Michael Adler <michael.adler@siemens.com>
+ */
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	"github.com/siemens/wfx/generated/model"
+	"github.com/siemens/wfx/internal/handler/job"
+	"github.com/siemens/wfx/internal/handler/job/events"
+	"github.com/siemens/wfx/internal/handler/job/status"
+	"github.com/siemens/wfx/internal/handler/workflow"
+	"github.com/siemens/wfx/workflow/dau"
+	"github.com/steinfletcher/apitest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJobEventsSubscribe(t *testing.T) {
+	log.Logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.Stamp})
+
+	db := newInMemoryDB(t)
+	wf := dau.DirectWorkflow()
+	_, err := workflow.CreateWorkflow(context.Background(), db, wf)
+	require.NoError(t, err)
+
+	north, south := createNorthAndSouth(t, db)
+
+	handlers := []http.Handler{north, south}
+	for i, name := range allAPIs {
+		handler := handlers[i]
+		t.Run(name, func(t *testing.T) {
+			clientID := "TestJobEventsSubscribe"
+
+			var jobID atomic.Pointer[string]
+
+			var wg sync.WaitGroup
+			expectedTags := []string{"tag1", "tag2"}
+			ch, _ := events.AddSubscriber(context.Background(), events.FilterParams{ClientIDs: []string{clientID}}, expectedTags)
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+
+				// wait for job created event
+				ev := <-ch
+				payload := ev.Args[0].(*events.JobEvent)
+				assert.Equal(t, events.ActionCreate, payload.Action)
+				assert.Equal(t, expectedTags, payload.Tags)
+				jobID.Store(&payload.Job.ID)
+
+				// wait for event created by our status.Update below
+				<-ch
+				// now our GET request should have received the response as well,
+				// add some extra time to be safe
+				time.Sleep(100 * time.Millisecond)
+				events.ShutdownSubscribers()
+			}()
+
+			_, err := job.CreateJob(context.Background(), db, &model.JobRequest{ClientID: clientID, Workflow: wf.Name})
+			require.NoError(t, err)
+
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				// wait for subscriber which is created by our GET request below and our test goroutine above
+				for events.SubscriberCount() != 2 {
+					time.Sleep(20 * time.Millisecond)
+				}
+				// update job
+				_, err = status.Update(context.Background(), db, *jobID.Load(), &model.JobStatus{State: "INSTALLING"}, model.EligibleEnumCLIENT)
+				require.NoError(t, err)
+			}()
+
+			// wait for job id
+			for jobID.Load() == nil {
+				time.Sleep(20 * time.Millisecond)
+			}
+
+			result := apitest.New().
+				Handler(handler).
+				Get("/api/wfx/v1/jobs/events").Query("ids", *jobID.Load()).
+				Expect(t).
+				Status(http.StatusOK).
+				Header("Content-Type", "text/event-stream").
+				End()
+
+			data, _ := io.ReadAll(result.Response.Body)
+			body := string(data)
+			require.NotEmpty(t, body)
+
+			lines := strings.Split(body, "\n")
+
+			t.Log("HTTP resonse body:")
+			for _, line := range lines {
+				t.Logf(">> %s", line)
+			}
+
+			assert.Len(t, lines, 4)
+
+			// check body starts with data:
+			assert.True(t, strings.HasPrefix(lines[0], "data: "))
+
+			// check content is a job and state is INSTALLING
+			var ev events.JobEvent
+			err = json.Unmarshal([]byte(strings.TrimPrefix(lines[0], "data: ")), &ev)
+			require.NoError(t, err)
+			assert.Equal(t, events.ActionUpdateStatus, ev.Action)
+			assert.Equal(t, "INSTALLING", ev.Job.Status.State)
+			assert.Equal(t, wf.Name, ev.Job.Workflow.Name)
+			assert.Equal(t, clientID, ev.Job.ClientID)
+			assert.Equal(t, "id: 1", lines[1])
+
+			wg.Wait()
+			events.ShutdownSubscribers()
+		})
+	}
+}

middleware/responder/sse/responder.go
@@ -1,0 +1,76 @@
+package sse
+
+/*
+ * SPDX-FileCopyrightText: 2023 Siemens AG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Author: Michael Adler <michael.adler@siemens.com>
+ */
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/olebedev/emitter"
+	"github.com/siemens/wfx/middleware/logging"
+)
+
+// Responder streams server-sent events (SSE) to a client.
+// It listens for events from the provided channel and dispatches them
+// to the client as soon as they arrive.
+//
+// Parameters:
+// - ctx: The context for managing the lifecycle of the stream. If canceled, streaming stops.
+// - source: A read-only channel of events to be transmitted.
+func Responder(ctx context.Context, source <-chan emitter.Event) middleware.ResponderFunc {
+	return func(w http.ResponseWriter, p runtime.Producer) {
+		log := logging.LoggerFromCtx(ctx)
+
+		flusher, _ := w.(http.Flusher)
+
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		flusher.Flush()
+
+		var id uint64 = 1
+	Loop:
+		for {
+			log.Debug().Msg("Waiting for next event")
+			select {
+			case ev, ok := <-source:
+				if !ok {
+					log.Debug().Msg("SSE channel is closed")
+					break Loop
+				}
+				b, err := json.Marshal(ev.Args[0])
+				if err != nil {
+					log.Error().Err(err).Msg("Failed to marshal status event")
+					continue Loop
+				}
+				log.Debug().RawJSON("event", b).Msg("Sending event to client")
+
+				_, _ = w.Write([]byte("data: "))
+				_, _ = w.Write(b)
+				// must end with two newlines as required by the SSE spec:
+				_, _ = w.Write([]byte(fmt.Sprintf("\nid: %d\n\n", id)))
+
+				flusher.Flush()
+
+				id++
+			case <-ctx.Done():
+				// this typically happens when the client closes the connection
+				log.Debug().Msg("Context is done")
+				break Loop
+			}
+		}
+		log.Info().Msg("Event Subscriber finished")
+	}
+}

middleware/responder/sse/responder_test.go
@@ -1,0 +1,55 @@
+package sse
+
+/*
+ * SPDX-FileCopyrightText: 2023 Siemens AG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Author: Michael Adler <michael.adler@siemens.com>
+ */
+
+import (
+	"context"
+	"net/http/httptest"
+	"sync"
+	"testing"
+
+	"github.com/olebedev/emitter"
+	"github.com/siemens/wfx/generated/model"
+	"github.com/siemens/wfx/internal/producer"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSSEResponder(t *testing.T) {
+	events := make(chan emitter.Event)
+
+	rw := httptest.NewRecorder()
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		responder := Responder(context.Background(), events)
+		responder.WriteResponse(rw, producer.JSONProducer())
+	}()
+
+	jobStatus := model.JobStatus{
+		ClientID: "foo",
+		Message:  "hello world",
+		State:    "INSTALLING",
+	}
+	events <- emitter.Event{
+		Topic:         "",
+		OriginalTopic: "",
+		Flags:         0,
+		Args:          []any{jobStatus},
+	}
+	close(events)
+
+	wg.Wait()
+
+	assert.Equal(t, `data: {"clientId":"foo","message":"hello world","state":"INSTALLING"}
+id: 1
+
+`, rw.Body.String())
+}

commit ecb08e72f9e8862982bead6a4416c19fc4a4d988

fix: do not close SSE connection due to inactivity
Job event connections were prematurely closed due to inactivity.

Fixes: 976875fab380ef85ece8c3b835a3ae4e891f9e70
Reported-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Michael Adler <michael.adler@siemens.com>

middleware/logging/writer.go
@@ -9,8 +9,11 @@ package logging
  */
 
 import (
+	"bufio"
 	"bytes"
+	"errors"
 	"io"
+	"net"
 	"net/http"
 
 	"github.com/Southclaws/fault"
@@ -22,6 +25,12 @@ type responseWriter struct {
 	httpWriter   http.ResponseWriter
 	statusCode   int
 }
+
+// responseWriter implements the following interfaces (compile-time check):
+var (
+	_ http.Flusher  = (*responseWriter)(nil)
+	_ http.Hijacker = (*responseWriter)(nil)
+)
 
 func newMyResponseWriter(w http.ResponseWriter, interceptBody bool) *responseWriter {
 	var result responseWriter
@@ -48,9 +57,21 @@ func (w *responseWriter) Write(b []byte) (int, error) {
 	return n, fault.Wrap(err)
 }
 
-// Flush implements the http.Flusher interface.
-// This is used by the server-sent events implementation to flush a single event to the client.
+// Flush is used by the server-sent events implementation to flush a single event to the client.
+// This is part of the http.Flusher interface.
 func (w *responseWriter) Flush() {
 	flusher := w.httpWriter.(http.Flusher)
 	flusher.Flush()
+}
+
+// Hijack allows an HTTP handler to take over the underlying connection.
+// This is used by the server-sent events implementation to keep the long-running (idle) connection open.
+// NOTE: The "funny" name comes from Golang's http.Hijacker interface.
+func (w *responseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	hj, ok := w.httpWriter.(http.Hijacker)
+	if !ok {
+		return nil, nil, errors.New("hijacker interface not supported")
+	}
+	conn, bw, err := hj.Hijack()
+	return conn, bw, fault.Wrap(err)
 }

middleware/sse/recorder.go
@@ -1,0 +1,52 @@
+//go:build testing
+
+package sse
+
+/*
+ * SPDX-FileCopyrightText: 2025 Siemens AG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Author: Michael Adler <michael.adler@siemens.com>
+ */
+
+import (
+	"bufio"
+	"io"
+	"net"
+	"net/http"
+	"net/http/httptest"
+)
+
+// MockResponseRecorder extends httptest.ResponseRecorder and implements http.Hijacker.
+type MockResponseRecorder struct {
+	*httptest.ResponseRecorder
+	hijackedConn net.Conn
+	ChResponse   chan string
+}
+
+// Ensure MockResponseRecorder implements http.Hijacker.
+var _ http.Hijacker = &MockResponseRecorder{}
+
+// NewMockResponseRecorder creates a new MockResponseRecorder.
+func NewMockResponseRecorder() *MockResponseRecorder {
+	return &MockResponseRecorder{
+		ResponseRecorder: httptest.NewRecorder(),
+		ChResponse:       make(chan string),
+	}
+}
+
+// Hijack implements the http.Hijacker interface.
+func (c *MockResponseRecorder) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	if c.hijackedConn == nil {
+		// Create a dummy connection for testing purposes.
+		var r net.Conn
+		c.hijackedConn, r = net.Pipe()
+		go func() {
+			response, _ := io.ReadAll(r)
+			c.ChResponse <- string(response)
+		}()
+	}
+	rw := bufio.NewReadWriter(bufio.NewReader(c.hijackedConn), bufio.NewWriter(c.hijackedConn))
+	return c.hijackedConn, rw, nil
+}

middleware/sse/responder.go
@@ -14,6 +14,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/Southclaws/fault"
 	"github.com/olebedev/emitter"
 	"github.com/siemens/wfx/middleware/logging"
 )
@@ -37,14 +38,33 @@ func NewResponder(ctx context.Context, eventChan <-chan emitter.Event) Responder
 func (responder Responder) VisitGetJobsEventsResponse(w http.ResponseWriter) error {
 	log := logging.LoggerFromCtx(responder.ctx)
 
-	flusher, _ := w.(http.Flusher)
+	// Check if the ResponseWriter supports hijacking
+	hj, ok := w.(http.Hijacker)
+	if !ok { // e.g. if HTTP/2 is being used
+		return fmt.Errorf("http.Hijacker interface not supported")
+	}
 
-	w.Header().Set("Content-Type", "text/event-stream")
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	// use raw connection to prevent it being closed by http.Server's idle cleanup routines
+	conn, bufrw, err := hj.Hijack()
+	if err != nil {
+		return fmt.Errorf("failed to hijack connection: %w", err)
+	}
+	defer func() {
+		_ = conn.Close()
+	}()
 
-	flusher.Flush()
+	_, _ = fmt.Fprintf(bufrw, "HTTP/1.1 %d\r\n", http.StatusOK)
+	_, _ = bufrw.WriteString("Content-Type: text/event-stream\r\n")
+	_, _ = bufrw.WriteString("Cache-Control: no-cache\r\n")
+	_, _ = bufrw.WriteString("Connection: keep-alive\r\n")
+	_, _ = bufrw.WriteString("Access-Control-Allow-Origin: *\r\n")
+	// finish header section
+	_, _ = bufrw.WriteString("\r\n")
+
+	if err := bufrw.Flush(); err != nil {
+		log.Err(err).Msg("Failed to send event to client")
+		return fault.Wrap(err)
+	}
 
 	var id uint64 = 1
 Loop:
@@ -63,12 +83,17 @@ Loop:
 			}
 			log.Debug().RawJSON("event", b).Msg("Sending event to client")
 
-			_, _ = w.Write([]byte("data: "))
-			_, _ = w.Write(b)
 			// must end with two newlines as required by the SSE spec:
-			_, _ = fmt.Fprintf(w, "\nid: %d\n\n", id)
+			_, err = fmt.Fprintf(bufrw, "data: %s\nid: %d\n\n", b, id)
+			if err != nil {
+				log.Err(err).Msg("Cannot write to buffer")
+				return fault.Wrap(err)
+			}
 
-			flusher.Flush()
+			if err := bufrw.Flush(); err != nil {
+				log.Err(err).Msg("Failed to send event to client")
+				return fault.Wrap(err)
+			}
 
 			id++
 		case <-responder.ctx.Done():

api/job_events_test.go
@@ -11,7 +11,7 @@ package api
 import (
 	"context"
 	"encoding/json"
-	"io"
+	"fmt"
 	"net/http"
 	"os"
 	"strings"
@@ -28,8 +28,8 @@ import (
 	"github.com/siemens/wfx/internal/handler/job/events"
 	"github.com/siemens/wfx/internal/handler/job/status"
 	"github.com/siemens/wfx/internal/handler/workflow"
+	"github.com/siemens/wfx/middleware/sse"
 	"github.com/siemens/wfx/workflow/dau"
-	"github.com/steinfletcher/apitest"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -94,26 +94,22 @@ func TestJobEventsSubscribe(t *testing.T) {
 				time.Sleep(20 * time.Millisecond)
 			}
 
-			result := apitest.New().
-				Handler(handler).
-				Get("/api/wfx/v1/jobs/events").Query("ids", *jobID.Load()).
-				Expect(t).
-				Status(http.StatusOK).
-				Header("Content-Type", "text/event-stream").
-				End()
+			req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("/api/wfx/v1/jobs/events?ids=%s", *jobID.Load()), nil)
+			rec := sse.NewMockResponseRecorder()
+			handler.ServeHTTP(rec, req)
 
-			data, _ := io.ReadAll(result.Response.Body)
-			body := string(data)
-			require.NotEmpty(t, body)
+			response := <-rec.ChResponse
+			assert.Contains(t, response, "HTTP/1.1 200")
+			assert.Contains(t, response, "Content-Type: text/event-stream")
 
-			lines := strings.Split(body, "\n")
-
-			t.Log("HTTP resonse body:")
+			lines := strings.Split(response, "\r\n")
+			t.Log("HTTP response:")
 			for _, line := range lines {
 				t.Logf(">> %s", line)
 			}
+			assert.Len(t, lines, 7)
 
-			assert.Len(t, lines, 4)
+			lines = strings.Split(lines[len(lines)-1], "\n")
 
 			// check body starts with data:
 			assert.True(t, strings.HasPrefix(lines[0], "data: "))

middleware/sse/responder_test.go
@@ -10,7 +10,6 @@ package sse
 
 import (
 	"context"
-	"net/http/httptest"
 	"sync"
 	"testing"
 
@@ -22,14 +21,18 @@ import (
 func TestSSEResponder(t *testing.T) {
 	events := make(chan emitter.Event)
 
-	rw := httptest.NewRecorder()
+	rw := NewMockResponseRecorder()
 	var wg sync.WaitGroup
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		responder := NewResponder(context.Background(), events)
-		_ = responder.VisitGetJobsEventsResponse(rw)
+		if err := responder.VisitGetJobsEventsResponse(rw); err != nil {
+			close(events)
+			t.Log("Received error from VisitGetJobsEventsResponse:", err)
+			t.Fail()
+		}
 	}()
 
 	clientID := "foo"
@@ -49,8 +52,10 @@ func TestSSEResponder(t *testing.T) {
 
 	wg.Wait()
 
-	assert.Equal(t, `data: {"clientId":"foo","message":"hello world","state":"INSTALLING"}
+	response := <-rw.ChResponse
+
+	assert.Contains(t, response, `data: {"clientId":"foo","message":"hello world","state":"INSTALLING"}
 id: 1
 
-`, rw.Body.String())
+`)
 }