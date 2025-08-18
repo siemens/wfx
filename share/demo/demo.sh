7b8ca09d60e0ddb1a75e1b0aaf5b389fb357fd32
Michael Adler <michael.adler@siemens.com>
2025-08-18 10:43:56 +0200

    chore(demo): add GIT_ROOT to PATH, check for binaries
    Signed-off-by: Michael Adler <michael.adler@siemens.com>

--- share/demo/demo.sh
+++ share/demo/demo.sh
@@ -1,13 +1,12 @@
-# SPDX-FileCopyrightText: 2023 Siemens AG
+# SPDX-FileCopyrightText: 2025 Siemens AG
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 # Author: Michael Adler <michael.adler@siemens.com>
-DM=$(command -v demo-magic.sh)
+DM=$(command -v demo-magic.sh || echo "")
 [ "$DM" != "" ] || {
     DM=$(mktemp)
-    echo "Saving demo-magic.sh to $DM"
-    curl -Ls -o "$DM" https://github.com/paxtonhare/demo-magic/raw/2a2f439c26a93286dc2adc6ef2a81755af83f36e/demo-magic.sh
+    curl -Ls -f -o "$DM" https://github.com/paxtonhare/demo-magic/raw/2a2f439c26a93286dc2adc6ef2a81755af83f36e/demo-magic.sh
     SHASUM=$(sha256sum "$DM" | awk '{print $1}')
     expected="ecaa937c89fe657668651610a62df6808daa3449248cdcbfe2be982b011dfb17"
     [ "$SHASUM" = "$expected" ] || {
@@ -15,5 +14,18 @@ DM=$(command -v demo-magic.sh)
         exit 1
     }
 }
+
+GIT_ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
+if [[ "$GIT_ROOT" != "" ]]; then
+    # extend PATH, in case wfx and wfxctl are stored in $GIT_ROOT
+    export PATH="$GIT_ROOT:$PATH"
+fi
+
+for binary in wfx wfxctl; do
+    command -v "$binary" >/dev/null || {
+        echo "FATAL: binary '$binary' not found"
+        exit 1
+    }
+done
 
 # vim: ft=sh
