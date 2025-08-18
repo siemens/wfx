c6b549b5cec16e91a91756b3ed231d95ea0f1f0b
Michael Adler <michael.adler@siemens.com>
2025-08-18 10:39:11 +0200

    chore(deps): update demo-magic
    Signed-off-by: Michael Adler <michael.adler@siemens.com>

--- share/demo/demo.sh
+++ share/demo/demo.sh
@@ -7,9 +7,9 @@ DM=$(command -v demo-magic.sh)
 [ "$DM" != "" ] || {
     DM=$(mktemp)
     echo "Saving demo-magic.sh to $DM"
-    curl -Ls -o "$DM" https://github.com/paxtonhare/demo-magic/raw/a938137035b73105d09347a91f9fd5e9722b617a/demo-magic.sh
+    curl -Ls -o "$DM" https://github.com/paxtonhare/demo-magic/raw/2a2f439c26a93286dc2adc6ef2a81755af83f36e/demo-magic.sh
     SHASUM=$(sha256sum "$DM" | awk '{print $1}')
-    expected="2f4f93fc8bc3c7708d51a12547c7f95024b4a49612191cdbc59233024d4b1cd3"
+    expected="ecaa937c89fe657668651610a62df6808daa3449248cdcbfe2be982b011dfb17"
     [ "$SHASUM" = "$expected" ] || {
         echo "checksum failed! actual: $SHASUM, expected: $expected"
         exit 1
