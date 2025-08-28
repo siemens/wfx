3fca7156a038aced37fac3b801daf011467cfc63
Michael Adler <michael.adler@siemens.com>
2025-08-28 10:44:25 +0200

    chore: add SPDX copyright identifier
    Signed-off-by: Michael Adler <michael.adler@siemens.com>

--- .ci/setup-build.sh
+++ .ci/setup-build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2025 Siemens AG
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Author: Michael Adler <michael.adler@siemens.com>
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
--- .ci/setup-cli-tests.sh
+++ .ci/setup-cli-tests.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2025 Siemens AG
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Author: Michael Adler <michael.adler@siemens.com>
 set -euo pipefail
 
 apt-get update -q
