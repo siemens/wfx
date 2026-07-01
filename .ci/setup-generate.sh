a85d48a62c12b96e24c1437e2d16007b05f81d10
Michael Adler <michael.adler@siemens.com>
2026-07-01 16:23:20 +0200

    ci: use just provided by distro
    Signed-off-by: Michael Adler <michael.adler@siemens.com>

--- .ci/setup-generate.sh
+++ .ci/setup-generate.sh
@@ -9,7 +9,6 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 apt-get update -q
-apt-get install -q -y --no-install-recommends python3-yaml git-lfs apt-transport-https unzip
+apt-get install -q -y --no-install-recommends python3-yaml git-lfs apt-transport-https unzip just
 
-"$SCRIPT_DIR/packages/just.sh"
 "$SCRIPT_DIR/packages/flatbuffers.sh"
--- .ci/setup-lint.sh
+++ .ci/setup-lint.sh
@@ -9,8 +9,7 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 apt-get update -q
-apt-get install -q -y --no-install-recommends zstd
+apt-get install -q -y --no-install-recommends zstd just
 
-"$SCRIPT_DIR/packages/just.sh"
 "$SCRIPT_DIR/packages/staticcheck.sh"
 "$SCRIPT_DIR/packages/golangci-lint.sh"
