f4d2293ac981b70d72521c3f7d35a5d231a4234a
Michael Adler <michael.adler@siemens.com>
2026-08-22 17:53:23 +0200

    chore: add missing SPDX copyright headers
    Signed-off-by: Michael Adler <michael.adler@siemens.com>

--- ui/integration/playwright.config.ts
+++ ui/integration/playwright.config.ts
@@ -1,3 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Siemens AG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Author: Michael Adler <michael.adler@siemens.com>
+ */
 import { defineConfig, devices } from "@playwright/test";
 
 /**
