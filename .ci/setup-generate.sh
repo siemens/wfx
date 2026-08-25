651495f9d35367fd7b223115237797991caa6403
Michael Adler <michael.adler@siemens.com>
2026-08-25 15:58:50 +0200

    ci: use latest version of just
    This reverts commit a85d48a62c12b96e24c1437e2d16007b05f81d10.
    
    Signed-off-by: Michael Adler <michael.adler@siemens.com>

--- .ci/setup-generate.sh
+++ .ci/setup-generate.sh
@@ -9,6 +9,7 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 apt-get update -q
-apt-get install -q -y --no-install-recommends python3-yaml git-lfs apt-transport-https unzip just
+apt-get install -q -y --no-install-recommends python3-yaml git-lfs apt-transport-https unzip
 
+"$SCRIPT_DIR/packages/just.sh"
 "$SCRIPT_DIR/packages/flatbuffers.sh"
