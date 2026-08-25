5d37a693b7672d32d84ee45189cddfdb51586388
Michael Adler <michael.adler@siemens.com>
2026-08-25 15:58:50 +0200

    ci: use latest version of just
    This reverts commit a85d48a62c12b96e24c1437e2d16007b05f81d10.
    
    Signed-off-by: Michael Adler <michael.adler@siemens.com>

--- .ci/setup-ui.sh
+++ .ci/setup-ui.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# SPDX-FileCopyrightText: 2025 Siemens AG
+# SPDX-FileCopyrightText: 2026 Siemens AG
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,9 +8,4 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-apt-get update -q
-apt-get install -q -y --no-install-recommends zstd
-
 "$SCRIPT_DIR/packages/just.sh"
-"$SCRIPT_DIR/packages/staticcheck.sh"
-"$SCRIPT_DIR/packages/golangci-lint.sh"
